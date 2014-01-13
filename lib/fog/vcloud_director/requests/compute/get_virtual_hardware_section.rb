66d507c527c197fae2d7d624b61ac2446f0c7198
Michal Jedryszka <Michal.Jedryszka@cybercom.com>
2014-01-13 09:45:56 +0100

    Improved working with hardware sections. Get hardware section can now return original xml without parsing or processed with any parser

--- lib/fog/vcloud_director/requests/compute/get_virtual_hardware_section.rb
+++ lib/fog/vcloud_director/requests/compute/get_virtual_hardware_section.rb
@@ -9,19 +9,31 @@ module Fog
         # VirtualHardwareSection individually, or as groups of related items.
         #
         # @param [String] id Object identifier of the VM.
+        # @param [Nokogiri::XML::SAX::Document] optional parser used for processing response. If nil supplied no parsing will be made
         # @return [Excon::Response]
         #   * body<~Hash>:
         #
         # @see http://pubs.vmware.com/vcd-51/topic/com.vmware.vcloud.api.reference.doc_51/doc/operations/GET-VirtualHardwareSection.html
         # @since vCloud API version 0.9
-        def get_virtual_hardware_section(id)
-          request(
+        def get_virtual_hardware_section(*args)
+          id = args[0]
+          parser = nil
+          if args.size == 1
+            parser = Fog::ToHashDocument.new
+          end
+          if args.size == 2
+            parser = args[1]
+          end
+          props = {
             :expects    => 200,
             :idempotent => true,
             :method     => 'GET',
-            :parser     => Fog::ToHashDocument.new,
             :path       => "vApp/#{id}/virtualHardwareSection/"
-          )
+          }
+          if parser
+            props[:parser] = parser
+          end
+          request(props)
         end
       end
     end
